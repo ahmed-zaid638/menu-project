cb262f4518f421fd3262dfdc9de064f4bc66571a
Ahmed <ahmedzaida464@gmail.com>
2022-06-03 15:36:01 +0200

    last updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Categories from './Categories';
 import items from './data';
 const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
